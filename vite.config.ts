bb6d729d4cc264974b80f72c03b58b04a7b6d84e
PI11A203-キムスミン <ellan122316@gmail.com>
2025-09-16 13:38:11 +0900

    スワイプファイル追加

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,18 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import tailwindcss from "@tailwindcss/vite"; //  추가
 
 export default defineConfig({
   plugins: [
     reactRouter(),
+    tailwindcss(), //  추가
     VitePWA({
       injectRegister: null, // SW 등록 코드 주입 금지
       includeAssets: ["favicon.ico", "icons/*"],
       manifest: false, // public/manifest.webmanifest 사용
       devOptions: { enabled: false }, // 개발 모드 SW 생성 비활성
-      filename: "pwa-sw.js", // 혹시 생성돼도 우리 sw.js와 이름 충돌 방지
+      filename: "pwa-sw.js", // 혹시 생성돼도 sw.js와 이름 충돌 방지
     }),
   ],
   server: { host: true, port: 5173, strictPort: true },
