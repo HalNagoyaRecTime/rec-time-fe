b7003cbd083f9b8e6fbe0e743b11969ab812a6d6
yukiidayo <yukiha617@gmail.com>
2025-09-28 10:33:30 +0900

    map画像の名前修正、ルートページへの遷移実装、スケジュールページ削除

--- app/components/devNavigation.tsx
+++ app/components/devNavigation.tsx
@@ -5,10 +5,10 @@ export default function DevNavigation() {
     return (
         <div className="TemporaryContainer fixed bottom-2 left-2 z-50 flex h-auto w-auto flex-col items-center gap-1 rounded-sm border-2 border-solid border-[#ff8d07] bg-black p-1">
             <Link to="/" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
-                <span className="text-white">home</span>
+                <span className="text-white">root</span>
             </Link>
-            <Link to="/schedule" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
-                <span className="text-white">schedule</span>
+            <Link to="/timetable" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
+                timetable
             </Link>
             <Link to="/id-input" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
                 <span className="text-white">id-input</span>
@@ -19,8 +19,8 @@ export default function DevNavigation() {
             <Link to="/map" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
                 map
             </Link>
-            <Link to="/timetable" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
-                timetable
+            <Link to="/home" className="cursor-pointer rounded-sm px-2 py-1 hover:bg-gray-800">
+                home
             </Link>
         </div>
     );
--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,9 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 
 export default [
-    { path: "/", file: "routes/timetable.tsx" },
+    { path: "/", file: "routes/_index.tsx" },
     { path: "/timetable", file: "routes/timetable.tsx" },
     { path: "/home", file: "routes/home.tsx" },
-    { path: "/schedule", file: "routes/schedule.tsx" },
     // 開発中
     { path: "/id-input", file: "routes/id-input.tsx" },
     { path: "/settings", file: "routes/settings.tsx" },
