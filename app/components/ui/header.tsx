924f39fb47cf4684b7e48320225d522b7ec187b5
yukiidayo <yukiha617@gmail.com>
2025-10-02 20:24:57 +0900

    修正

--- app/components/ui/header.tsx
+++ app/components/ui/header.tsx
@@ -23,7 +23,7 @@ export default function header({ onMenuOpen }: HeaderProps) {
 
     return (
         // 最大幅を取得するための要素
-        <header className="sticky flex h-14 w-full items-end gap-5 px-6">
+        <header className="sticky flex h-14 w-full items-center gap-5 bg-blue-800 px-6">
             <button onClick={onMenuOpen} className="h-10 w-10 cursor-pointer p-2 text-white">
                 <img src="/icons/app-icon/hamburger-menu.png" alt="" />
             </button>
