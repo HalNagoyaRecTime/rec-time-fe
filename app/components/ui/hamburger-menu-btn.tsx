272875de9476f9dad1f98201137ee5ce6ac6ebc7
yukiidayo <yukiha617@gmail.com>
2025-10-11 17:56:13 +0900

    ハンバーガーメニューボタンが必ず真ん中に配置されるように調整

--- app/components/ui/hamburger-menu-btn.tsx
+++ app/components/ui/hamburger-menu-btn.tsx
@@ -9,35 +9,37 @@ export default function HamburgerMenuBtn({ onClick, isOpen }: HamburgerMenuBtnPr
     return (
         <button
             onClick={onClick}
-            className="absolute top-4 left-4 !z-99 flex h-10 w-10 cursor-pointer flex-col items-center justify-center gap-1 p-2"
+            className="absolute top-0 !z-99 flex h-17 w-17 cursor-pointer items-center justify-center"
         >
-            <div
-                className={`h-1 w-6 rounded-full transition-all duration-300 ${isOpen ? "translate-y-[8px] rotate-45" : ""}`}
-                style={{
-                    backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
-                    // WebkitBackgroundClip: "text",
-                    WebkitTextFillColor: "transparent",
-                    // backgroundClip: "text",
-                }}
-            />
-            <div
-                className={`h-1 w-6 rounded-full bg-white transition-all duration-300 ${isOpen ? "opacity-0" : ""}`}
-                style={{
-                    backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
-                    // WebkitBackgroundClip: "text",
-                    WebkitTextFillColor: "transparent",
-                    // backgroundClip: "text",
-                }}
-            />
-            <div
-                className={`h-1 w-6 rounded-full bg-white transition-all duration-300 ${isOpen ? "-translate-y-[8px] -rotate-45" : ""}`}
-                style={{
-                    backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
-                    // WebkitBackgroundClip: "text",
-                    WebkitTextFillColor: "transparent",
-                    // backgroundClip: "text",
-                }}
-            />
+            <div className="flex flex-col gap-1">
+                <div
+                    className={`h-1 w-6 rounded-full transition-all duration-300 ${isOpen ? "translate-y-[8px] rotate-45" : ""}`}
+                    style={{
+                        backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
+                        // WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        // backgroundClip: "text",
+                    }}
+                />
+                <div
+                    className={`h-1 w-6 rounded-full bg-white transition-all duration-300 ${isOpen ? "opacity-0" : ""}`}
+                    style={{
+                        backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
+                        // WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        // backgroundClip: "text",
+                    }}
+                />
+                <div
+                    className={`h-1 w-6 rounded-full bg-white transition-all duration-300 ${isOpen ? "-translate-y-[8px] -rotate-45" : ""}`}
+                    style={{
+                        backgroundImage: `linear-gradient(90deg, #fec42e, #fce6a0)`,
+                        // WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent",
+                        // backgroundClip: "text",
+                    }}
+                />
+            </div>
         </button>
     );
 }
