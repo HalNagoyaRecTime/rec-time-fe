3afdac1e280d04e4ab1c67413c9561a74937ff59
kaketo0113 <m.kaketo113@gmail.com>
2025-10-24 11:59:51 +0900

    iOS PWAのステータスバーの色を白に変更

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
             <head>
                 <meta charSet="utf-8" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <meta name="theme-color" content="#000D91" />
+                <meta name="theme-color" content="#ffffff" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
                 <title>recTime</title>
                 <Meta />
                 <Links />
